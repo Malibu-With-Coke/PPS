e4f2f5229b57e8709fde3e01c3e17cca864e1e9f
JungGyeom <love@handong.ac.kr>
2022-07-14 14:44:06 +0900

    07.14

--- C/030.py
+++ C/030.py
@@ -1,4 +1,18 @@
 n = int(input())
 m = int(input())
 
-up
+up = (m-n) + (n-1)
+down = n-1
+
+x = up
+y = down
+
+for i in range(up-1, up-down, -1):
+    up *= i
+for i in range(down-1, 1, -1):
+    down *= i
+
+if down==0:
+    print(1)
+else:
+    print(int(up/down))
